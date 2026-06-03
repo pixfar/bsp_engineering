a1de17503e48d66331c6b210d561ca775ca8677a
Shaid Azmin <excel.azmin@gmail.com>
2026-06-03 07:24:24 +0000

    add item group on requsition

--- bsp_engineering/bsp_engineering/doctype/requisition/requisition.js
+++ bsp_engineering/bsp_engineering/doctype/requisition/requisition.js
@@ -1,4 +1,25 @@
 // Copyright (c) 2026, Pixfar and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Requisition", {});
+frappe.ui.form.on("Requisition", {
+	refresh: function (frm) {
+		frm.fields_dict["items"].grid.get_field("item_code").get_query = function (
+			doc,
+			cdt,
+			cdn
+		) {
+			let row = frappe.get_doc(cdt, cdn);
+			if (row.item_group) {
+				return { filters: { item_group: row.item_group } };
+			}
+			return {};
+		};
+	},
+});
+
+frappe.ui.form.on("Requisition Item", {
+	item_group: function (frm, cdt, cdn) {
+		// Clear item_code when group changes so stale value isn't kept
+		frappe.model.set_value(cdt, cdn, "item_code", "");
+	},
+});
