3abf1a7493febab3a66615b55d200f437cd5ece1
Shaid Azmin <excel.azmin@gmail.com>
2026-08-29 17:05:45 +0000

    Add Viewer role only

--- bsp_engineering/hooks.py
+++ bsp_engineering/hooks.py
@@ -36,7 +36,8 @@ fixtures = [
     { "dt": "Print Format", "filters": [["name", "like", "BSP%"]] },
     { "dt": "Workflow State" },
     { "dt": "Workflow" },
-    { "dt": "Role", "filters": [["name", "in", ["BSP Admin"]]] },
+    { "dt": "Role", "filters": [["name", "in", ["BSP Admin", "BSP Viewer"]]] },
+    { "dt": "Custom DocPerm", "filters": [["role", "=", "BSP Viewer"]] },
 ]
 
 # Each item in the list will be shown as an app in the apps page
